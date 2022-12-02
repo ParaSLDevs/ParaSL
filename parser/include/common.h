dfb7fe63bf5f7b0373831ad3237ee300c1111d8d
aakmaev <akmaev.alexey@huawei-partners.com>
2022-12-02 13:37:11 +0300

    Fix boost parser issues for compilation

--- parser/include/common.h
+++ parser/include/common.h
@@ -9,10 +9,14 @@
 #include <boost/spirit/include/support_extended_variant.hpp>
 
 #include <boost/spirit/include/qi.hpp>
+#include <boost/phoenix.hpp>
+
+#include "error_handler.h"
 
 namespace parasl {
 
 namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
 
 using qi::char_;
 using qi::string;
@@ -26,6 +30,17 @@ using qi::alpha;
 using qi::alnum;
 using qi::_val_type;
 
+using qi::_1;
+using qi::_2;
+using qi::_3;
+using qi::_4;
+
+using qi::on_error;
+using qi::fail;
+
+using phx::function;
+
+using StrIter = std::string::iterator;
 using PrimType = std::variant<int, char, double, float>;
 using IntType = std::variant<int8_t, int16_t, int32_t, int64_t>;
 
@@ -42,43 +57,57 @@ using IntType = std::variant<int8_t, int16_t, int32_t, int64_t>;
 //    base_type::variant_type const & getNode() const { return base_type::get(); }
 //};
 //using node_t = boost::variant<
-////        std::string, int, char, double, float, int8_t, int16_t, int32_t, int64_t, _val_type
-////        , PrimType
-////        , IntType
 //        SyntaxNode,
 //        boost::recursive_wrapper<SyntaxNode>
 //>;
-template <typename T>
-class node_t {
-public:
-    typedef T value_type;
 
-    T x;
-};
-
+using node_t = ChildSyntaxNode<> *;
+//class node_t {
+//public:
+//    using value_type = SyntaxNode *;
+//    value_type node;
+//
+//    node_t() : node(nullptr) {}
+//    node_t(const std::string& str) : node(nullptr) {}
+//    node_t(const int& i) : node(nullptr) {}
+//    node_t(StrIter beg, StrIter end) : node(nullptr) {}
+//
+//    bool empty() const {
+//        return true;
+//    }
+//
+//    void insert(StrIter it, char val) {
+//
+//    }
+//
+//    StrIter end() {
+//        return {};
+//    }
+//};
 
 //using node_t = boost::make_recursive_variant<
 //        std::string, std::vector<boost::recursive_variant_> >::type;
 
+
 struct keywords_table : qi::symbols<char> {
     keywords_table() {
         add
-            ("layer")
-            ("input")
-            ("output")
-            ("repeat")
-            ("glue")
-            ("bind")
-            ("if")
-            ("else")
-            ("for")
-            ("in")
-            ("return")
-            ("char")
-            ("int")
-            ;
+                ("layer")
+                ("input")
+                ("output")
+                ("repeat")
+                ("glue")
+                ("bind")
+                ("if")
+                ("else")
+                ("for")
+                ("in")
+                ("return")
+                ("char")
+                ("int")
+                ;
     }
-} keywords_t;
+};
 
 
 }  // namespace parasl
--- parser/include/stmt_grammar.h
+++ parser/include/stmt_grammar.h
@@ -10,37 +10,48 @@
 
 namespace parasl {
 
-template<typename Iterator>
-struct stmt_grammar : qi::grammar<Iterator, node_t(), Skipper<Iterator>> {
-    stmt_grammar() : stmt_grammar::base_type{STMTS} {
-        STMTS = 
-                STMT 
-                | STMTS
-                ;
-        
-        STMT =
-                INIT_VAR_WITH_TYPE_STMT >> char_(';')
-                | EXPR >> char_(';')
-                ;
+// int_ [ std::cout << qi::_1 << std::endl]
+//STMTS =
+//STMT [ std::cout << qi::_1 << std::endl]
+//| STMTS [ std::cout << qi::_1 << std::endl]
+//;
 
-        INIT_VAR_WITH_TYPE_STMT =
-                NAME >> char_(':') >> VAR_TYPE
-                | NAME >> char_(':') >> VAR_TYPE >> char_('=') >> EXPR
-                ;
-
-        VAR_TYPE =
-                VAR_BUILTIN_TYPE
-                | VAR_TYPE_WITH_BIT_SIZE
-                ;
-
-        VAR_TYPE_WITH_BIT_SIZE = qi::int_ >> '(' >> qi::int_ >> ')';
-
-        VAR_BUILTIN_TYPE = qi::int_ | qi::char_ | qi::double_ | qi::float_;
+template<typename Iterator, typename Skipper>
+struct stmt_grammar : qi::grammar<Iterator, node_t(), Skipper> {
+    stmt_grammar(error_handler<Iterator>& error_handler) :
+                stmt_grammar::base_type{STMTS}, EXPR(error_handler) {
+        using error_handler_function = function<parasl::error_handler<Iterator>>;
 
         NAME =
                 !lexeme[keywords_t >> !(alnum | '_')]
                 >>  raw[lexeme[(alpha | '_') >> *(alnum | '_')]]
                 ;
+
+        VAR_TYPE_WITH_BIT_SIZE = lit("int") >> '(' >> qi::int_ >> ')';
+
+        VAR_BUILTIN_TYPE = lit("int") | lit("char") | lit("double") | lit("float");
+
+        VAR_TYPE =
+                VAR_TYPE_WITH_BIT_SIZE
+                | VAR_BUILTIN_TYPE
+                ;
+
+        INIT_VAR_WITH_TYPE_STMT =
+                NAME >> char_(':') >> VAR_TYPE [qi::_1]
+                | NAME >> char_(':') >> VAR_TYPE >> char_('=') >> EXPR [qi::_1]
+                ;
+
+        INIT_VAR_WITHOUT_TYPE_STMT =
+                NAME >> char_('=') >> EXPR [std::cout << qi::_1 << std::endl]
+                ;
+
+        STMT =
+                INIT_VAR_WITH_TYPE_STMT >> char_(';')
+                | INIT_VAR_WITHOUT_TYPE_STMT >> char_(';')
+                | EXPR >> char_(';')
+                ;
+
+        STMTS = +STMT [qi::_1];
 
         BOOST_SPIRIT_DEBUG_NODES(
                 (INIT_VAR_WITH_TYPE_STMT)
@@ -49,16 +60,20 @@ struct stmt_grammar : qi::grammar<Iterator, node_t(), Skipper<Iterator>> {
                 (VAR_BUILTIN_TYPE)
                 (NAME)
         )
+
+        // Error handling: on error in STMTS, call error_handler.
+        on_error<fail>(STMTS,
+                       error_handler_function(error_handler)(
+                               "Error! Expecting ", _4, _3));
     }
 
 private:
-    expression_grammar<Iterator> EXPR;
-    qi::rule<Iterator, node_t(), Skipper<Iterator>>
-//            INIT_VAR_WITH_TYPE_STMT, VAR_TYPE, STMT, STMTS;
-            NAME, INIT_VAR_WITH_TYPE_STMT, VAR_TYPE, VAR_TYPE_WITH_BIT_SIZE, VAR_BUILTIN_TYPE, STMT, STMTS;
-//    qi::rule<Iterator, PrimType(), Skipper<Iterator>> VAR_BUILTIN_TYPE;
-//    qi::rule<Iterator, IntType(), Skipper<Iterator>> VAR_TYPE_WITH_BIT_SIZE;
-//    qi::rule<Iterator, std::string(), Skipper<Iterator>> NAME;
+    keywords_table keywords_t;
+    expression_grammar<Iterator, Skipper> EXPR;
+
+    qi::rule<Iterator, node_t(), Skipper>
+            INIT_VAR_WITH_TYPE_STMT, INIT_VAR_WITHOUT_TYPE_STMT, STMT, STMTS;
+    qi::rule<Iterator, std::string(), Skipper> NAME, VAR_TYPE_WITH_BIT_SIZE, VAR_BUILTIN_TYPE, VAR_TYPE;
 };
 
 }  // namespace parasl
