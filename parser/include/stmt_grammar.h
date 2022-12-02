0bf5ccce1f7fb49ffb8caaa2bb5eacd1b81e779d
aakmaev <akmaev.alexey@huawei-partners.com>
2022-12-02 16:50:22 +0300

    Fix expression grammar

--- parser/include/stmt_grammar.h
+++ parser/include/stmt_grammar.h
@@ -10,53 +10,54 @@
 
 namespace parasl {
 
-// int_ [ std::cout << qi::_1 << std::endl]
-//STMTS =
-//STMT [ std::cout << qi::_1 << std::endl]
-//| STMTS [ std::cout << qi::_1 << std::endl]
-//;
-
 template<typename Iterator, typename Skipper>
 struct stmt_grammar : qi::grammar<Iterator, node_t(), Skipper> {
     stmt_grammar(error_handler<Iterator>& error_handler) :
                 stmt_grammar::base_type{STMTS}, EXPR(error_handler) {
-        using error_handler_function = function<parasl::error_handler<Iterator>>;
+        using error_handler_function = function<parasl::error_handler<Iterator> >;
+
+        VAR_BUILTIN_TYPE.add
+                ("int")
+                ("char")
+                ("float")
+                ("double")
+                ;
 
         NAME =
-                !lexeme[keywords_t >> !(alnum | '_')]
+                    !lexeme[keywords_t >> !(alnum | '_')]
                 >>  raw[lexeme[(alpha | '_') >> *(alnum | '_')]]
                 ;
 
-        VAR_TYPE_WITH_BIT_SIZE = lit("int") >> '(' >> qi::int_ >> ')';
-
-        VAR_BUILTIN_TYPE = lit("int") | lit("char") | lit("double") | lit("float");
+        VAR_TYPE_WITH_BRACKETS =
+                    (lit("int") >> '(') > qi::int_ > ')'          // int with size decl
+                |   (VAR_BUILTIN_TYPE >> '[') > qi::int_ > ']'    // arrays decl
+                ;
 
         VAR_TYPE =
-                VAR_TYPE_WITH_BIT_SIZE
-                | VAR_BUILTIN_TYPE
+                    VAR_TYPE_WITH_BRACKETS
+                |   VAR_BUILTIN_TYPE
                 ;
 
-        INIT_VAR_WITH_TYPE_STMT =
-                NAME >> char_(':') >> VAR_TYPE [qi::_1]
-                | NAME >> char_(':') >> VAR_TYPE >> char_('=') >> EXPR [qi::_1]
-                ;
-
-        INIT_VAR_WITHOUT_TYPE_STMT =
-                NAME >> char_('=') >> EXPR [std::cout << qi::_1 << std::endl]
+        INIT_VAR =
+                    (NAME >> ':') > VAR_TYPE  // var decl with type
+                >>  *('=' > EXPR)             [qi::_1]
+                |   (NAME >> '=')             // var decl without type
+                >   EXPR                      [qi::_1]
                 ;
 
         STMT =
-                INIT_VAR_WITH_TYPE_STMT >> char_(';')
-                | INIT_VAR_WITHOUT_TYPE_STMT >> char_(';')
-                | EXPR >> char_(';')
+                    INIT_VAR > char_(';')
+                |   EXPR > char_(';')
                 ;
 
-        STMTS = +STMT [qi::_1];
+        STMTS = +STMT
+                [qi::_1]
+                ;
 
         BOOST_SPIRIT_DEBUG_NODES(
-                (INIT_VAR_WITH_TYPE_STMT)
+                (INIT_VAR)
                 (VAR_TYPE)
-                (VAR_TYPE_WITH_BIT_SIZE)
+                (VAR_TYPE_WITH_BRACKETS)
                 (VAR_BUILTIN_TYPE)
                 (NAME)
         )
@@ -64,16 +65,17 @@ struct stmt_grammar : qi::grammar<Iterator, node_t(), Skipper> {
         // Error handling: on error in STMTS, call error_handler.
         on_error<fail>(STMTS,
                        error_handler_function(error_handler)(
-                               "Error! Expecting ", _4, _3));
+                               "Error while statement parsing! Expecting ", _4, _3));
     }
 
 private:
     keywords_table keywords_t;
     expression_grammar<Iterator, Skipper> EXPR;
 
-    qi::rule<Iterator, node_t(), Skipper>
-            INIT_VAR_WITH_TYPE_STMT, INIT_VAR_WITHOUT_TYPE_STMT, STMT, STMTS;
-    qi::rule<Iterator, std::string(), Skipper> NAME, VAR_TYPE_WITH_BIT_SIZE, VAR_BUILTIN_TYPE, VAR_TYPE;
+    SymGroup VAR_BUILTIN_TYPE;
+
+    qi::rule<Iterator, node_t(), Skipper> INIT_VAR, STMT, STMTS;
+    qi::rule<Iterator, std::string(), Skipper> NAME, VAR_TYPE_WITH_BRACKETS, VAR_TYPE;
 };
 
 }  // namespace parasl
