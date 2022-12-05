d4d08e5f32aebbb47591bff46d68eb3ac6ac70b9
FixPlus <bushev.da@phystech.edu>
2022-12-06 02:05:56 +0300

    [Parser] Added actions to grammar rules
    Grammar was also refactored

--- parser/include/layer0_grammar_def.h
+++ parser/include/layer0_grammar_def.h
@@ -17,31 +17,32 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
      */
 
     UNARY_OP.add
-            ("+")
-            ("-")
-            ("!")
+            ("+", UnaryOp::PLUS)
+            ("-", UnaryOp::MINUS)
+            ("!", UnaryOp::INV)
             ;
 
     MULT_OP.add
-            ("*")
-            ("/")
+            ("*", MultOp::MULT)
+            ("/", MultOp::DIV)
             ;
 
     ADD_OP.add
-            ("+")
-            ("-")
+            ("+", AddOp::ADD)
+            ("-", AddOp::SUB)
+
             ;
 
     RELATION_OP.add
-            ("<")
-            ("<=")
-            (">")
-            (">=")
+            ("<", RelOp::LT)
+            ("<=", RelOp::LE)
+            (">", RelOp::GT)
+            (">=", RelOp::GE)
             ;
 
     EQUALITY_OP.add
-            ("==")
-            ("!=")
+            ("==", EqOp::EQ)
+            ("!=", EqOp::NE)
             ;
 
     NAME =
@@ -66,52 +67,51 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
             ;
 
     TERM =
-                qi::uint_                                  [qi::_1]
+                qi::uint_                                  [ASTBuilder::IntegralLiteral()]
             |   FUNC_CALL
             |   SQUARE_BRAKET_EXPR
             |   DOT_EXPR
-            |   NAME                                       [qi::_1]
+            |   NAME                                       [ASTBuilder::StringLiteral()]
             |   ENTITY_EXPR                                       [qi::_1]
             |   '(' > EXPR > ')'
             ;
 
     UNARY_EXPR =
-                TERM
-            |   (UNARY_OP > TERM)
+                TERM                                [ASTBuilder::Terminal()]
+            |   (UNARY_OP > TERM)                   [ASTBuilder::TerminalWithUnaryOp()]
             ;
 
     MULT =
-                UNARY_EXPR
-            >>  *(MULT_OP > UNARY_EXPR)
+            (UNARY_EXPR
+            >>  *(MULT_OP > UNARY_EXPR))             [ASTBuilder::Multiplication()]
             ;
 
     ADD_OR_MINUS_EXPR =
-                MULT
-            >>  *(ADD_OP > MULT)
+            (MULT
+            >>  *(ADD_OP > MULT))               [ASTBuilder::Addition()]
             ;
 
     LESS_OR_GREATER_EXPR =
-                ADD_OR_MINUS_EXPR
-            >>  *(RELATION_OP > ADD_OR_MINUS_EXPR)
+            (ADD_OR_MINUS_EXPR
+            >>  *(RELATION_OP > ADD_OR_MINUS_EXPR))            [ASTBuilder::Relation()]
             ;
 
     EQUALITY_EXPR =
-                LESS_OR_GREATER_EXPR
-            >>  *(EQUALITY_OP > LESS_OR_GREATER_EXPR)
+            (LESS_OR_GREATER_EXPR
+            >>  *(EQUALITY_OP > LESS_OR_GREATER_EXPR))           [ASTBuilder::Equality()]
             ;
 
     AND_EXPR =
-                EQUALITY_EXPR
-            >>  *("&&" > EQUALITY_EXPR)
+            (EQUALITY_EXPR
+            >>  *("&&" > EQUALITY_EXPR))                       [ASTBuilder::LogicAnd()]
             ;
 
     OR_EXPR =
-                AND_EXPR
-            >>  *("||" > AND_EXPR)
+            (AND_EXPR
+            >>  *("||" > AND_EXPR))                             [ASTBuilder::LogicOr()]
             ;
 
-    EXPR =
-                OR_EXPR
+    EXPR =  OR_EXPR
             //>>  *("=" > OR_EXPR)
             ;
 
@@ -198,7 +198,7 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
      */
 
     OUTPUT_STMT =
-                (lit("output") > '(' > int_(0) >> ',' > EXPR > ')') /* EXPR, not NAME? */  [qi::_1]
+                (lit("output") > '(' > int_(0) >> ',' > EXPR > ')') /* EXPR, not NAME? */  [ASTBuilder::OutputStatement()]
             ;
 
     LOOP_IF_BODY =
@@ -207,12 +207,12 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
             ;
 
     RANGE =
-                (int_ >> ':') > int_ > -(':' > int_)                                       [qi::_1]
+            ((int_ >> ':') > int_ > -(':' > int_))                                       [ASTBuilder::Range()]
             ;
 
     LOOP_HEADER =
-                (lit("for") > '(' > NAME > lit("in") > (NAME | RANGE) > ')')               [qi::_1]
-            |   (lit("while") > '(' > EXPR > ')')                                          [qi::_1]
+                (lit("for") > '(' > NAME > lit("in") > (NAME | RANGE) > ')')               [ASTBuilder::ForStatement()]
+            |   (lit("while") > '(' > EXPR > ')')                                          [ASTBuilder::WhileStatement()]
             ;
 
     LOOP_STMT =
@@ -220,36 +220,38 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
             ;
 
     IF_STMT =
-                lit("if") > '(' > EXPR > ')'
+            (lit("if") > '(' > EXPR > ')'
             >   LOOP_IF_BODY
-            >   -(lit("else") > LOOP_IF_BODY)
+            >   -(lit("else") > LOOP_IF_BODY))          [ASTBuilder::IfStatement()]
             ;
 
     VAR_TYPE_WITH_BRACKETS =
-                ((lit("int") >> '(') > qi::int_ > ')')       [qi::_1]     // int with size decl
+                ((lit("int") >> '(') > qi::int_ > ')')       [ASTBuilder::IntegralTypeWithBitwidth()]     // int with size decl
             ;
 
     ARR_TYPE =
                 (((VAR_BUILTIN_TYPE | STRUCT_TYPE | FUNC_TYPE) >> '[')
-            > uint_ > ']')                                                                 [qi::_1]
+            > uint_ > ']')                                                                 [ASTBuilder::ArrayType()]
 
-            |   (lit("vector") > '<' > VAR_BUILTIN_TYPE > ',' > uint_ > '>')               [qi::_1]
+            |   (lit("vector") > '<' > VAR_BUILTIN_TYPE > ',' > uint_ > '>')               [ASTBuilder::VectorType()]
             ;
 
     VAR_TYPE =
                 VAR_TYPE_WITH_BRACKETS
-            |   VAR_BUILTIN_TYPE >> !(char_('(') | '[')                        [qi::_1]
+            |   (VAR_BUILTIN_TYPE >> !(char_('(') | '[') )
             |   ARR_TYPE
             |   STRUCT_TYPE
             |   FUNC_TYPE
             ;
 
+    VAR_BUILTIN_TYPE = VAR_BUILTIN_TYPES [ASTBuilder::BuiltInType()];
+
     DECL_EXPR =
-                (NAME >> ':' >> VAR_TYPE)                         [qi::_1]
+                (NAME >> ':' >> VAR_TYPE)                         [ASTBuilder::Declaration()]
             ;
 
     STRUCT_TYPE =
-                ('{' >> -(DECL_EXPR % ',') > '}')                         [qi::_1]
+                ('{' >> -(DECL_EXPR % ',') > '}')                         [ASTBuilder::StructType()]
             ;
 
     FUNC_TYPE =
@@ -260,8 +262,8 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
             ;
 
     ASSIGNMENT =
-                (DECL_EXPR | EXPR)                             // var decl with type
-            >>  *('=' >> EXPR)                                                           [qi::_1]
+            ((DECL_EXPR | EXPR)                             // var decl with type
+            >>  *('=' >> EXPR))                                            [ASTBuilder::Assignment()]
             ;
 
     STMT =
@@ -272,8 +274,7 @@ layer0_grammar<Iterator, Skipper>::layer0_grammar(error_handler<Iterator>& error
             |   DECL_EXPR > ';'
             ;
 
-    STMTS = +STMT
-            [qi::_1]
+    STMTS = (*STMT)                   [ASTBuilder::CompoundStatement()]
             ;
 
     BOOST_SPIRIT_DEBUG_NODES(
